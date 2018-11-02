5690511ded3459b563e572ecea4e2c04548feef4
arslan16021992 <44335378+arslan16021992@users.noreply.github.com>
2018-11-02 18:54:27 +0700

    1

--- SandBox/src/main/java/Arslan/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/Arslan/sandbox/MyFirstProgram.java
@@ -1,7 +1,10 @@
 package Arslan.sandbox;
 
 public class MyFirstProgram {
-public static void main(String[] args){
-	System.out.println("Hello word!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello word!");
+        System.out.println(2+2);
+        System.out.println(2*2);
+
+    }
 }
