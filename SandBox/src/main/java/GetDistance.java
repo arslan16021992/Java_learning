2220874bfeb77bbfa32563a0a02407781dd6e626
arslan16021992 <44335378+arslan16021992@users.noreply.github.com>
2018-11-13 21:44:26 +0700

    Расчет расстояния между двумя точками c использованием метода и конструктора
    Расчет расстояния между двумя точками c использованием метода и конструктора, так же конструкции this

--- SandBox/src/main/java/GetDistance.java
+++ SandBox/src/main/java/GetDistance.java
@@ -1,18 +1,16 @@
 public class GetDistance {
     public static void main (String args[]){
-        Point p1=new Point();
-        p1.x=6;
-        p1.y=8;
+        Point p1=new Point(6,8);
+        //p1.x=6;
+       //p1.y=8;
 
-        Point p2=new Point();
-        p2.x=6;
-        p2.y=7;
+        Point p2=new Point(1,3);
+        //p2.x=6;
+        //p2.y=7;
 
-        System.out.println("Расстояние между точками A("+p1.x+"; "+p1.y+") и B("+p2.x+"; "+p2.y+") равно "+ distance(p1, p2));
+        System.out.println("Расстояние между точками A("+p1.x+"; "+p1.y+") и B("+p2.x+"; "+p2.y+") равно "+ p1.distance(p2));
     }
-    public static double distance(Point p1, Point p2){
-        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
-    }
+
 }
 
 
--- SandBox/src/main/java/Point.java
+++ SandBox/src/main/java/Point.java
@@ -1,4 +1,14 @@
 public class Point {
     public double x;
     public double y;
+
+    public Point(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public double distance(Point p2){
+        double sqrt = Math.sqrt(Math.pow(this.x - p2.x, 2) + Math.pow(this.y - p2.y, 2));
+        return sqrt;
+    }
 }
